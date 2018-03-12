78d6fd91f77318985cffaecf4d7aa45aaf550f95
Mark Stahl <markstahl@Marks-MacBook-Pro.local>
2018-03-12 09:05:38 -0400

    First commit

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,34 @@
+class AnswersController < ApplicationController
+  def index
+    @question = Question.find(params[:question_id])
+    @answers = Answer.order(updated_at: :desc)
+  end
+
+  def new
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new
+    @errors =[]
+  end
+
+  def create
+    @question = Question.find_by(id: params[:question_id])
+    @answer = Answer.new(answer_params)
+    @answer.question = @question
+
+    if @answer.save
+      redirect_to @question, notice: 'Answer was successfully created.'
+    else
+      @answers = @question.answers
+      @errors = @answer.errors.full_messages
+      render '/questions/show'
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:description)
+  end
+
+
+end
---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,61 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.order(updated_at: :desc)
+  end
+
+  def show
+    @question = Question.find_by(id: params[:id])
+    @answers = @question.answers
+    @answer = Answer.new
+    @errors = []
+  end
+
+  def new
+    @question = Question.new
+    @errors =[]
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+    @errors = []
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to @question, notice: 'Question was successfully updated.'
+    else
+      @errors = @question.errors.full_messages
+      render :edit
+    end
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to @question, notice: 'Question was successfully created.'
+    else
+      @errors = @question.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @answers = @question.answers
+    @question.destroy
+    @answers.each do |answer|
+      answer.destroy
+    end
+
+    redirect_to questions_path, notice: 'Article Deleted'
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :description)
+  end
+
+end
